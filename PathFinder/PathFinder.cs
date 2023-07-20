156228d2a9eae4dcad419c2d8036bbf8e8a63141
Tomas Gužauskas <tomas.guzauskas@gmail.com>
2023-07-20 23:41:42 +0300

    Rewrite path finder to use dictionaries Refactor

--- PathFinder/PathFinder.cs
+++ PathFinder/PathFinder.cs
@@ -1,136 +1,97 @@
-﻿namespace PathFinder;
+﻿
+namespace PathFinder;
 
 public class PathFinder
 {
+    private static readonly int _transferCityMinutes = 30;
+
     /// <param name="from">Departure</param>
     /// <param name="to">Destination</param>
     /// <param name="pathsCSV">Times (hours and minutes) to travel between cities e.g. "Paris,Barcelona,9:40"</param>
-    public async Task<(TimeSpan, string)> FindFastestPath(string from, string to, string pathsCSV)
+    public (TimeSpan, string) FindFastestPath(string from, string to, string pathsCSV)
     {
-        var pathsMap = new List<string[]>();
-        var reader = new StringReader(pathsCSV);
-        var line = reader.ReadLine();
-        while (!string.IsNullOrEmpty(line))
+        var graph = CreateGraphFromCsv(pathsCSV);
+        var allPaths = FindAllConnectedPaths(from, to, new List<string>(), graph);
+
+        return FindShortestGraphPath(graph, allPaths);
+    }
+
+    private static Dictionary<string, Dictionary<string, int>> CreateGraphFromCsv(string csvContent)
+    {
+        var graph = new Dictionary<string, Dictionary<string, int>>();
+
+        foreach (var line in csvContent.Split('\n'))
         {
-            var i = line.IndexOf(',');
-            var j = 0;
-            var data = new string[3];
-            while (i != -1)
+            var parts = line.Split(',');
+            var city1 = parts[0];
+            var city2 = parts[1];
+            var distance = (int)TimeSpan.Parse(parts[2]).TotalMinutes;
+
+            if (!graph.ContainsKey(city1))
+                graph[city1] = new Dictionary<string, int>();
+
+            if (!graph.ContainsKey(city2))
+                graph[city2] = new Dictionary<string, int>();
+
+            graph[city1][city2] = distance;
+            graph[city2][city1] = distance;
+        }
+        return graph;
+    }
+    
+    private static List<List<string>> FindAllConnectedPaths(string from, string to, List<string> currentPath, Dictionary<string, Dictionary<string, int>> graph)
+    {
+        List<List<string>> allPaths = new List<List<string>>();
+
+        // Add the current city to the current path
+        currentPath.Add(from);
+
+        if (from == to)
+        {
+            // We have reached the destination city, add the current path to the result
+            allPaths.Add(new List<string>(currentPath));
+        }
+        else if (graph.ContainsKey(from))
+        {
+            foreach (var neighbor in graph[from])
             {
-                data[j] = line.Substring(0, i);
-                j = j + 1;
-                line = line.Substring(i + 1);
-                i = line.IndexOf(',');
-                if (i < 0)
-                    data[j] = line;
+                if (!currentPath.Contains(neighbor.Key)) // Avoid cycles
+                {
+                    // Recursive call to explore paths from the neighbor city
+                    List<List<string>> pathsFromNeighbor = FindAllConnectedPaths(neighbor.Key, to, currentPath, graph);
+
+                    // Add all paths found from the neighbor to the result
+                    allPaths.AddRange(pathsFromNeighbor);
+                }
             }
-            pathsMap.Add(data);
-            line = reader.ReadLine();
         }
 
-        var paths = new List<List<string[]>>();
+        // Remove the current city from the current path before backtracking
+        currentPath.Remove(from);
 
-        foreach (var mainPath in pathsMap)
-        {
-            if (mainPath[0] == from &&
-                mainPath[1] == to)
-            {
-                paths.Add(new List<string[]>{mainPath});
-                continue;
-            }
-            else if (mainPath[0] == to &&
-                     mainPath[1] == from)
-            {
-                paths.Add(new List<string[]>{new []{mainPath[1], mainPath[0], mainPath[2]}});
-                continue;
-            }
+        return allPaths;
+    }
 
-            var newPath = new List<string[]>();
-            if (mainPath[0] == from)
-            {
-                newPath.Add(mainPath);
-                paths.AddRange(FindDeeperPath(mainPath[1], to, newPath, pathsMap));
-            }
-            else if (mainPath[1] == from)
-            {
-                newPath.Add(new []{mainPath[1], mainPath[0], mainPath[2]});
-                paths.AddRange(FindDeeperPath(mainPath[0], to, newPath, pathsMap));
-            }
-        }
-
+    private static (TimeSpan, string) FindShortestGraphPath(Dictionary<string, Dictionary<string, int>> graph, List<List<string>> paths)
+    {
         (TimeSpan, string) shortestPath = (new TimeSpan(), "");
         foreach (var foundPath in paths)
         {
             TimeSpan runningTime = new TimeSpan();
-            int i = 0;
-            foreach (var pathDistance in foundPath)
+            for(var i = 0; i < foundPath.Count - 1; i++)
             {
-                runningTime = runningTime + TimeSpan.Parse(pathDistance[2]);
-                if (i > 0)
-                    runningTime = runningTime + TimeSpan.FromMinutes(30);
-
-                i++;
+                var time = graph[foundPath[i]][foundPath[i + 1]];
+                runningTime += TimeSpan.FromMinutes(time);
             }
+
+            runningTime += TimeSpan.FromMinutes(_transferCityMinutes * (foundPath.Count - 2)); //time for every transfer city
+
 
             if (runningTime < shortestPath.Item1 || shortestPath.Item1.Minutes == 0)
             {
-                shortestPath = (runningTime, "");
-                var separator = "";
-                var destination = "";
-                foreach (var path in foundPath)
-                {
-                    shortestPath.Item2 += separator + path[0];
-                    separator = ",";
-                    destination = path[1];
-                }
-                shortestPath.Item2 += separator + destination;
+                shortestPath = (runningTime, string.Join(',', foundPath));
             }
         }
-
         return shortestPath;
-    }
-
-    private List<List<string[]>> FindDeeperPath(string from, string to, List<string[]> path, List<string[]> pathsMap)
-    {
-        var paths = new List<List<string[]>>();
-        foreach (var mapPath in pathsMap)
-        {
-            if (path.Any(item => item[0] == mapPath[0]))
-            {
-                continue;
-            }
-            else if (mapPath[0] == from &&
-                     mapPath[1] == to)
-            {
-                var newPath = new List<string[]>(path);
-                newPath.Add(mapPath);
-                paths.Add(newPath);
-                continue;
-            }
-            else if (mapPath[0] == to &&
-                     mapPath[1] == from)
-            {
-                var newPath = new List<string[]>(path);
-                newPath.Add(new []{mapPath[1], mapPath[0], mapPath[2]});
-                paths.Add(newPath);
-                continue;
-            }
-            else if (mapPath[0] == from)
-            {
-                var newPath = new List<string[]>(path);
-                newPath.Add(mapPath);
-                paths.AddRange(FindDeeperPath(mapPath[1], to, newPath, pathsMap));
-                continue;
-            }
-            else if (mapPath[1] == from)
-            {
-                var newPath = new List<string[]>(path);
-                newPath.Add(new []{mapPath[1], mapPath[0], mapPath[2]});
-                paths.AddRange(FindDeeperPath(mapPath[0], to, newPath, pathsMap));
-                continue;
-            }
-        }
-
-        return paths;
     }
 }
--- PathFinderTests/UnitTest1.cs
+++ PathFinderTests/UnitTest1.cs
@@ -5,20 +5,22 @@ using System.Threading.Tasks;
 [TestFixture]
 public class PathFinderTest
 {
-    private string _ltPath = @"
-Klaipeda,Kaunas,2:00
+    private string _ltPath = 
+@"Klaipeda,Kaunas,2:00
 Vilnius,Kaunas,1:00
 Kaunas,Ukmerge,1:00
 Vilnius,Ukmerge,1:00
 Klaipeda,Panevezys,3:00
-Panevezys,Vilnius,2:00";
+Panevezys,Vilnius,2:00
+Vilnius,Utena,1:00
+Utena,Klaipeda,5:00";
   
     [Test]
-    public async Task FindFastestPath_LTPathArray_Success()
+    public void FindFastestPath_LTPathArray_Success()
     {
         var finder = new PathFinder.PathFinder();
-        var result = await finder.FindFastestPath("Vilnius", "Klaipeda", _ltPath);
-        Assert.AreEqual(TimeSpan.FromMinutes(210), result.Item1);
-        Assert.AreEqual("Vilnius,Kaunas,Klaipeda", result.Item2);
+        var result = finder.FindFastestPath("Vilnius", "Klaipeda", _ltPath);
+        Assert.That(result.Item1, Is.EqualTo(TimeSpan.FromMinutes(210)));
+        Assert.That(result.Item2, Is.EqualTo("Vilnius,Kaunas,Klaipeda"));
     }
 }
